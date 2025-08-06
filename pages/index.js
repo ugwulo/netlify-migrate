0690f0a49abfee9f94db068a08806e8b81733d6e
Johnpaul Ugwulo <seanlite94@gmail.com>
2025-08-06 11:33:40 +0100

    add Auth

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,9 @@ import { projects } from '../utils/projectsData';
 const Home = () => (
   <div className="home">
     <h1>What Can Johnpaul Deploy to Static Apps?</h1>
-    <h1><a href="/.auth/login/aad">Login</a></h1>
+    <h1>
+      <a href="/.auth/login/aad">Login</a>
+    </h1>
     <div className="card-grid">
       {projects.map((project) => {
         const Icon = projectIcons[project.id];
